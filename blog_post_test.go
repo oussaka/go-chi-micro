040bb98d064f3cb04abb5a5d04b5254ddc3f7a25
oussaka <kaabachi.dev@gmail.com>
2025-05-08 11:45:04 +0200

    fix unit tests

--- blog_post_test.go
+++ blog_post_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/oussaka/go-chi-micro/handler"
+	"github.com/oussaka/go-chi-micro/httphandler"
 	"github.com/oussaka/go-chi-micro/model"
 	"io/ioutil"
 	"net/http"
@@ -12,35 +13,43 @@ import (
 	"testing"
 )
 
-var fakePosts = []*model.Post{{
-	ID:               "1",
-	Title:            "7 habits of highly effective people",
-	Author:           "Stephen Covey",
-	PublishedDate:    "15/08/1989",
-	OriginalLanguage: "english",
+var fakeBlogPosts = []*model.Blogs{{
+	ID:              1,
+	BlogName:        "Lorem ipsum dolor.",
+	BlogDetails:     "Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id. Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean.",
+	BlogDescription: "Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem.",
 }}
+
+var BlogData = []model.BlogData{{
+	Blog:    *fakeBlogPosts[0],
+	Message: "OK",
+}}
+
+var fakeResponse = httphandler.Response{
+	Data: *fakeBlogPosts[0],
+}
 
 type fakeStorage struct {
 }
 
-func (s fakeStorage) Get(_ string) *model.Post {
-	return fakePosts[0]
+func (s fakeStorage) Get(_ string) (model.Blogs, error) {
+	return *fakeBlogPosts[0], nil
 }
 
-func (s fakeStorage) Delete(_ string) *model.Post {
-	return fakePosts[0]
+func (s fakeStorage) Delete(_ string) (string, error) {
+	return "OK deleted", nil
 }
 
-func (s fakeStorage) List() []*model.Post {
-	return fakePosts
+func (s fakeStorage) List() []*model.Blogs {
+	return fakeBlogPosts
 }
 
-func (s fakeStorage) Create(_ model.Post) {
-	return
+func (s fakeStorage) Create(_ *model.Blogs) (model.BlogData, error) {
+	return BlogData[0], nil
 }
 
-func (s fakeStorage) Update(_ string, _ model.Post) *model.Post {
-	return fakePosts[1]
+func (s fakeStorage) Update(_ string, _ *model.Blogs) (model.BlogData, error) {
+	return BlogData[0], nil
 }
 
 func TestGetPostsHandler(t *testing.T) {
@@ -65,13 +74,17 @@ func TestGetPostsHandler(t *testing.T) {
 	// Check the response code
 	checkResponseCode(t, http.StatusOK, res.StatusCode)
 
-	Post := model.Post{}
-	json.Unmarshal(data, &Post)
-	if Post.Title != "7 habits of highly effective people" {
-		t.Errorf("expected ABC got %v", string(data))
+	//Post := model.Post{}
+	//json.Unmarshal(data, &Post)
+
+	json.Unmarshal(data, &fakeResponse)
+
+	var returnedBlogName = fakeResponse.Data.(map[string]interface{})["blog_name"]
+	if returnedBlogName != "Lorem ipsum dolor." {
+		t.Errorf("expected 'Lorem ipsum dolor.' got %v", returnedBlogName)
 	}
-	if Post.ID != "1" {
-		t.Errorf("expected Post ID = 1 got %v", Post.ID)
+	if fakeResponse.Data.(map[string]interface{})["id"] != float64(1) {
+		t.Errorf("expected Post ID = 1 got %v", fakeResponse.Data.(map[string]interface{})["id"])
 	}
 }
 
--- model/model.go
+++ model/model.go
@@ -14,6 +14,24 @@ type ErrResponse struct {
 	AppCode        int64        `json:"code,omitempty"` // application-specific error code
 }
 
+//type Post struct {
+//	ID               string `json:"id"`
+//	Title            string `json:"title"`
+//	Author           string `json:"author"`
+//	PublishedDate    string `json:"published_date"`
+//	OriginalLanguage string `json:"original_language"`
+//}
+//
+//var Posts = []*Post{
+//	{
+//		ID:               "1",
+//		Title:            "7 habits of highly effective people",
+//		Author:           "Stephen Covey",
+//		PublishedDate:    "15/08/1989",
+//		OriginalLanguage: "english",
+//	},
+//}
+
 type Blogs struct {
 	ID              int    `json:"id" gorm:"primaryKey"`
 	BlogName        string `json:"blog_name"`
